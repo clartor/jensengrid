e63b4ad8fe98e947a1568f293d9dcfcc71feef52
clartor <clara.tornblom@gmail.com>
2021-03-05 18:18:35 +0100

    APi hämtat yay

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,52 @@
 <template>
+
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <div v-if="responseAvailable == true">
+      <hr />
+      <p>
+        <i>{{ result }}</i>
+      </p>
+      <hr />
+    </div>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  data: () => {
+    return {
+      result: '',
+      responseAvailable: false,
+      // apiKey: 'Top-Secret-Shhh-Key',
+    };
   },
-
-  data: () => ({
-    //
-  }),
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.responseAvailable = false;
+      fetch("https://www.foaas.com/bag/Clara", {
+        "method": "GET",
+      })
+        .then((response) => {
+          if (response.ok) {
+            response.text().then((response) => {
+              this.result = response;
+              this.responseAvailable = true;
+              console.log("this.result: " + this.result);
+            
+            });
+          } else {
+            alert(
+              "Server returned " + response.status + " : " + response.statusText
+            );
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
 };
 </script>
